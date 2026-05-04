c7cfeb5d6b6b91c54e7c32df743d5347d99aaee3
SC450 <shc068@ucsd.edu>
2026-05-04 01:19:23 -0700

    Test pre-commit

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 def main():
     print("Hello from dsc190-tools-assignment-5!")
 
+
 def foo(a, b, c, d, e, f, g, h, i, j, k, lst, m, n, o, p):
     lst = []
     if a:
         pass
     return lst
 
+
 if __name__ == "__main__":
     main()
     x = 1 + 2
     y = {"key": "value"}
-    
