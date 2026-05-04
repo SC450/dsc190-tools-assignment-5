67bda5403cc3136597a730232278d34ec170eb15
SC450 <shc068@ucsd.edu>
2026-05-04 01:01:09 -0700

    Fixed broken things

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import os
-import sys
-import json  # F401: imported but unused
-
 def main():
     print("Hello from dsc190-tools-assignment-5!")
 
-def foo(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p):  # E501 if long enough + too many args
-    l = []  # E741: ambiguous variable name 'l'
-    if a == True:  # E712: comparison to True
+def foo(a, b, c, d, e, f, g, h, i, j, k, lst, m, n, o, p):
+    lst = []
+    if a:
         pass
-    return l
+    return lst
 
 if __name__ == "__main__":
     main()
-    x=1+2  # E225: missing whitespace around operator
-    y = {'key' : 'value'}  # E203: whitespace before ':'
+    x = 1 + 2
+    y = {"key": "value"}
     
